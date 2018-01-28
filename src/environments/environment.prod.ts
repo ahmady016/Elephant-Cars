b5089f33e6746f90429d881b91d6dc441a1096e0
ahmady016 <ahmady09@gmail.com>
2018-01-28 04:16:40 +0200

    add firebase config object into prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCpQBLhbEFl59SorgxUbN4m1wO5bUfUYis",
+    authDomain: "elephant-cars-21b9e.firebaseapp.com",
+    databaseURL: "https://elephant-cars-21b9e.firebaseio.com",
+    projectId: "elephant-cars-21b9e",
+    storageBucket: "elephant-cars-21b9e.appspot.com",
+    messagingSenderId: "852000481036"
+  }
 };
